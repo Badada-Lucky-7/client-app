972640e9e9516be51921f7ad820a90210094e749
99mini <itman390@gmail.com>
2024-11-09 02:38:08 +0900

    feat: dynamically import SeoulMap component to prevent server-side rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 
-import SeoulMap from '@/components/SeoulMap';
+import dynamic from 'next/dynamic';
+
+const MapWithNoSSR = dynamic(() => import('../components/SeoulMap'), {
+  ssr: false,
+});
 
 const Home = () => {
   return (
     <main>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main page.</p>
-      <SeoulMap />
+      <MapWithNoSSR />
     </main>
   );
 };
