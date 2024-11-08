b09c8ebf6922f4eea21c922fe45cfff929e74511
99mini <itman390@gmail.com>
2024-11-09 03:04:35 +0900

    refactor: add GeoJsonLayer component and integrate it into SeoulMap

--- src/components/home/SeoulMap.tsx
+++ src/components/home/SeoulMap.tsx
@@ -1,56 +1,13 @@
 'use client';
 
 import { LatLngExpression } from 'leaflet';
-import { useEffect, useState } from 'react';
-import { GeoJSON, MapContainer, TileLayer } from 'react-leaflet';
+import { useState } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+
+import { SggType } from '@/types/Geo';
+import GeoJsonLayer from './GeoJsonLayer';
 
 import 'leaflet/dist/leaflet.css';
-
-type SggType = {
-  sgg: string;
-  sggnm: string;
-};
-
-const GeoJsonLayer = ({ onChange }: { onChange: (sgg: SggType | null) => void }) => {
-  const [geoData, setGeoData] = useState(null);
-
-  useEffect(() => {
-    fetch('/geo/seoul_sggnm.geojson')
-      .then((response) => response.json())
-      .then((data) => setGeoData(data))
-      .catch((error) => console.error('GeoJSON data loading error:', error));
-  }, []);
-
-  if (!geoData) {
-    return null;
-  }
-
-  return (
-    <GeoJSON
-      data={geoData}
-      style={{
-        color: 'blue',
-        weight: 2,
-        fillColor: 'lightblue',
-        fillOpacity: 0.3,
-      }}
-      onEachFeature={(feature, layer) => {
-        layer.on({
-          mouseover: () => {
-            onChange({
-              sgg: feature.properties.sgg as string,
-              sggnm: feature.properties.sggnm as string,
-            });
-            console.log(feature.properties);
-          },
-          mouseout: () => {
-            onChange(null);
-          },
-        });
-      }}
-    />
-  );
-};
 
 const center: LatLngExpression = [37.5665, 126.978]; // 서울의 위도와 경도
 
---
+++ src/types/Geo.ts
@@ -0,0 +1,4 @@
+export type SggType = {
+  sgg: string;
+  sggnm: string;
+};
