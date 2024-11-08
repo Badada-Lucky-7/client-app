2dd7a11e8ca97e6594d68d47dd64a0c942747c56
99mini <43674669+99mini@users.noreply.github.com>
2024-11-08 23:13:26 +0900

    feat: sign up page (#5)
    * chore: add login page and update navigation to include login link
    
    * chore: create Signup component with initial structure
    
    * feat: create SignUp page and Form component with initial structure

---
+++ src/components/sign-up/Form.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import './Form.css';
+
+const Form = () => {
+  return (
+    <div>
+      <form className="sign-up-form" action=""></form>
+    </div>
+  );
+};
+
+export default Form;
