4b74a5d7fef8783c0c4e4e8269bcce569572026d
99mini <itman390@gmail.com>
2024-11-08 21:47:51 +0900

    chore: update API routes to use environment variables for server host

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,7 @@
+import axios from 'axios';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  const data = { message: `Hello from API! ${request.body ?? ''}` };
-  return NextResponse.json(data);
+  const res = axios.get(`${process.env.NEXT_PUBLIC_SERVER_HOST}`);
+  return NextResponse.json(res);
 }
--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  const res = await axios.get(`https://api.plankton-hackathon.com/api/test/3`);
+  const res = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_HOST}/test/3`);
   return NextResponse.json(res);
 }
 
 export async function POST(request: NextRequest) {
   console.log('post');
-  const res = await axios.post(`https://api.plankton-hackathon.com/api/test`);
+  const res = await axios.post(`${process.env.NEXT_PUBLIC_SERVER_HOST}/test`);
   return NextResponse.json(res);
 }
