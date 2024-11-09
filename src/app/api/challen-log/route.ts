2a93d2207cc372de098b7550bd89b9d7b6855f4b
99mini <itman390@gmail.com>
2024-11-10 03:47:42 +0900

    feat: streamline API request in ChallenLog by removing unused search parameters

--- src/app/api/challen-log/route.ts
+++ src/app/api/challen-log/route.ts
@@ -14,15 +14,10 @@ export async function GET(request: NextRequest) {
     const { authorization } = await getCookieData();
 
     const searchParams = request.nextUrl.searchParams;
-    const id = searchParams.get('id');
-    const nickName = searchParams.get('nickName');
     const challengeId = searchParams.get('challengeId');
-    const text = searchParams.get('text');
-    const maxCount = searchParams.get('maxCount');
-    const likeCount = searchParams.get('likeCount');
 
     const res = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_HOST}/gathering`, {
-      params: { id, nickName, challengeId, text, maxCount, likeCount },
+      params: { challengeId },
       headers: {
         Authorization: authorization,
       },
