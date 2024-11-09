f6b1eaabd07b08b0d00605a5df063d027886c12c
99mini <43674669+99mini@users.noreply.github.com>
2024-11-10 03:40:23 +0900

    Update src/app/login/page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,6 @@ import { Skeleton } from '@mui/material';
 import { Suspense } from 'react';
 
 const Login = () => {
-  const token = session.get();
   return (
     <Suspense fallback={<Skeleton variant="rectangular" width={'100vw'} height={'100vh'} />}>
       <Form />
