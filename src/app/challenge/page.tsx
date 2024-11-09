121548d860a66434138936746c7343175afd158d
99mini <itman390@gmail.com>
2024-11-10 01:51:37 +0900

    fix: hotfix

--- src/app/challenge/page.tsx
+++ src/app/challenge/page.tsx
@@ -1,10 +1,14 @@
+'use client';
+
 import MissionCard from '@/components/mission/MissionCard';
+import { Skeleton } from '@mui/material';
+import { Suspense } from 'react';
 
 const Mission = () => {
   return (
-    <div>
+    <Suspense fallback={<Skeleton variant="rectangular" width={'100vw'} height={'100vh'} />}>
       <MissionCard />
-    </div>
+    </Suspense>
   );
 };
 
