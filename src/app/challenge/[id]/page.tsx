1b7cad0f66e69c97bc7744d591e0c23e3904ec52
Junyong <lhimjy1105@naver.com>
2024-11-09 20:36:05 +0900

    before merge

--- src/app/challenge/[id]/page.tsx
+++ src/app/challenge/[id]/page.tsx
@@ -1,7 +1,13 @@
 const Challenge = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  return <div>{id}</div>;
+  return (
+    <div>
+      {id}
+      <br />
+      <a href="./blog/page.tsx">Blog</a>
+    </div>
+  );
 };
 
 export default Challenge;
