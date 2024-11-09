5dc3629382c9e108490eff68fb329a4ed2b5f230
99mini <itman390@gmail.com>
2024-11-10 02:44:42 +0900

    feat: add suspense loading for ChallenLog component and improve layout structure

--- src/app/challen-log/page.tsx
+++ src/app/challen-log/page.tsx
@@ -1,24 +1,28 @@
 import ChallenLogCard from '@/components/challen-log/chllenLogCard';
 import ReviewCard from '@/components/review-card/ReviewCard';
 import SetModal from '@/components/write/setModal';
+import { Skeleton } from '@mui/material';
+import { Suspense } from 'react';
 import './challen-log.css';
 
 const ChallenLog = () => {
   return (
-    <div className="container">
-      <span className="reviewCard">
-        <div className="titlebar">
-          <span className="title" style={{ fontSize: 17, fontWeight: 550 }}>
-            이미 챌린지 성공한 사람의 글
-          </span>
-          <SetModal />
+    <Suspense fallback={<Skeleton variant="rectangular" width={'100vw'} height={'100vh'} />}>
+      <div className="container">
+        <span className="reviewCard">
+          <div className="titlebar">
+            <span className="title" style={{ fontSize: 17, fontWeight: 550 }}>
+              이미 챌린지 성공한 사람의 글
+            </span>
+            <SetModal />
+          </div>
+          <ReviewCard />
+        </span>
+        <div className="logCard">
+          <ChallenLogCard />
         </div>
-        <ReviewCard />
-      </span>
-      <div className="logCard">
-        <ChallenLogCard />
       </div>
-    </div>
+    </Suspense>
   );
 };
 export default ChallenLog;
