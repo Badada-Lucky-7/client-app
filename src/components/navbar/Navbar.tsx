02047b7c9f43b345133c4e50f3d7dd251668ae16
99mini <itman390@gmail.com>
2024-11-09 20:12:50 +0900

    feat: update Navbar component layout and styling, add logo image

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import useProfile from '@/hooks/useProfile';
+import Image from 'next/image';
 import Link from 'next/link';
 
 import './Navbar.css';
@@ -12,12 +13,16 @@ const Navbar = () => {
     <header className="navbar-header">
       <nav className="navbar-nav">
         <Link href="/" className="nav-logo">
-          홈
+          <Image src="/asset/logo.svg" alt="logo" width={274} height={120} />
         </Link>
-        <Link href="/challen-log" className="nav-challen-log">
-          {'challen-log'}
-        </Link>
-        {profile ? <div>{profile.email}</div> : <Link href="/login">{'로그인'}</Link>}
+        <div className="navbar-leftside">
+          <Link href="/challen-log" className="nav-link">
+            {'challen-log'}
+          </Link>
+          <Link href={profile ? 'profile' : '/login'} className="nav-link">
+            {'로그인'}
+          </Link>
+        </div>
       </nav>
     </header>
   );
